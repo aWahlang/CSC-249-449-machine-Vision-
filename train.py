7e5753ab716d607328a62c1434ae3ed4ab86d8a3
Daniel Barnett <daniel@daniel-latitude.dbarnett.me>
2018-05-07 13:57:59 -0400

    updated code

--- train.py
+++ train.py
@@ -13,12 +13,13 @@ import time
 
 parser = argparse.ArgumentParser(description='PyTorch Emotion Training')
 parser.add_argument('--model', default='ShuffleNet' , help='model: ShuffleNet or MobileNet')
-parser.add_argument('--lr', default=0.1, type=float, help='initial learning rate')
+parser.add_argument('--lr', default=0.01, type=float, help='initial learning rate')
 
 args = parser.parse_args()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+print("device:")
 print(device)
 
 model = ShuffleNet()
@@ -29,10 +30,13 @@ if args.model == 'MobileNet' :
 
 model.to(device)
 
+print("loading data...")
+
+
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
 trainset = torchvision.datasets.ImageFolder(root='./outputs/train', transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, shuffle=True, batch_size=16, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, shuffle=True, batch_size=64, num_workers=2)
 
 testset = torchvision.datasets.ImageFolder(root='./outputs/val', transform=transform)
 testloader = torch.utils.data.DataLoader(testset, shuffle=True, batch_size=1)
@@ -40,7 +44,6 @@ testloader = torch.utils.data.DataLoader(testset, shuffle=True, batch_size=1)
 classes = ('anger', 'contempt', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise', 'uncertain')
 
 #summary(model, (3,224,224))
-
 
 criterion = nn.CrossEntropyLoss()
     
@@ -58,13 +61,13 @@ for epoch in range(90):  # loop over the dataset multiple times
     
     model.train(True)
     
-    adjust_learning_rate(optimizer, epoch)
-
+    #adjust_learning_rate(optimizer, epoch)
+    print("training data:")
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs
         inputs, labels = data
-        
+
         inputs, labels = inputs.to(device), labels.to(device)
 
         # wrap them in Variable
@@ -81,11 +84,9 @@ for epoch in range(90):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 1000 == 0:    # print every 2000 mini-batches
+        if i % 500 == 0:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
             running_loss = 0.0
-            
-            
 
 
     print('finished epoch %d' % epoch)
@@ -107,19 +108,19 @@ for epoch in range(90):  # loop over the dataset multiple times
     for data in testloader:
         count +=1
         image, label = data
-        
-        
+
+
         image, labels = image.to(device), labels.to(device)
         output = model(Variable(image))
         _, predicted = torch.max(output.data, 1)
         predicted = predicted.to("cpu")
         total += 1
         class_total[label[0]] +=1
-        
+
         if (label[0] == predicted[0]):
             correct += 1
             class_correct[predicted[0]] += 1
-        
+
     elapsed_time = time.time() - start_time
     accuracy = (100 * correct / total)
     print('Accuracy of the network on the test images: %d%%' % accuracy)
@@ -132,25 +133,3 @@ for epoch in range(90):  # loop over the dataset multiple times
     if accuracy > max_accuracy :
         torch.save(model, args.model + ".pt")
         max_accuracy = accuracy
-
-
-
-    #class_correct = list(0. for i in range(9))
-    #class_total = list(0. for i in range(9))
-    #for data in testloader:
-        #images, labels = data
-        ##print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-        #outputs = model(Variable(images))
-        #_, predicted = torch.max(outputs.data, 1)
-        ##print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
-        #c = (predicted == labels).squeeze()
-        #for i in range(4):
-            #label = labels[i]
-            #class_correct[label] += c[i]
-            #class_total[label] += 1
-
-
-    
-        
-
-
